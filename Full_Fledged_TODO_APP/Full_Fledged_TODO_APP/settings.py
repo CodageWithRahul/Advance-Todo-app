3994e705ad0d2b2316b2d25c604df520bdef0b05
CodageWithRahul <135108397+CodageWithRahul@users.noreply.github.com>
2026-01-29 18:01:35 +0530

    make changes in settings

--- Full_Fledged_TODO_APP/Full_Fledged_TODO_APP/settings.py
+++ Full_Fledged_TODO_APP/Full_Fledged_TODO_APP/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rot52rb(71ew-z5ae$e-v!42(5xh7)n$gtqr6izi6d6d07@gqb"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-rot52rb(71ew-z5ae$e-v!42(5xh7)n$gtqr6izi6d6d07@gqb"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 ALLOWED_HOSTS = ["*"]
 
@@ -120,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 REST_FRAMEWORK = {
