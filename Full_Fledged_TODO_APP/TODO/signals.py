c3a0e1b1ab316a8d8eb77409c0f49d36d402fc70
CodageWithRahul <135108397+CodageWithRahul@users.noreply.github.com>
2026-01-29 23:54:40 +0530

    welcome email update

--- Full_Fledged_TODO_APP/TODO/signals.py
+++ Full_Fledged_TODO_APP/TODO/signals.py
@@ -5,19 +5,22 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
 
-@receiver(post_save,sender=User)
-def sendWelcomeMail(sender,instance,created,**kwargs):
+
+@receiver(post_save, sender=User)
+def sendWelcomeMail(sender, instance, created, **kwargs):
     if created:
-        sub = f'Welcome {instance.first_name} To our Platform'
-        bodymess = render_to_string('email/welcomeEmail.html',{
-            'fname': instance.first_name,
-            'lname': instance.last_name,
-            
-        })
-        
-        from_email = settings.EMAIL_HOST_USER
+        sub = f"Welcome {instance.first_name} To our Platform"
+        bodymess = render_to_string(
+            "email/welcomeEmail.html",
+            {
+                "fname": instance.first_name,
+                "lname": instance.last_name,
+            },
+        )
+
+        from_email = settings.DEFAULT_FROM_EMAIL
         recipient = [instance.email]
-        
-        email = EmailMessage(sub,bodymess,from_email,recipient)
-        email.content_subtype = 'html'
+
+        email = EmailMessage(sub, bodymess, from_email, recipient)
+        email.content_subtype = "html"
         email.send()
